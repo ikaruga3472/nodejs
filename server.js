dca531fc1bdee57e1487ca5f2df4cc0d9afd2a7a
Byung-yun <quddbstmvls@gmail.com>
2025-04-18 16:22:59 +0900

    0418

--- server.js
+++ server.js
@@ -10,3 +10,13 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
  
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb사이트에 있던 님들의 DB 접속 URL'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+}).catch((err)=>{
+  console.log(err)
+})
